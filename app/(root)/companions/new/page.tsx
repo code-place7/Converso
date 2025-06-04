f8c41780b7c1ebc3da03967c68c5c1d9dec4818d
MONU MANDAL <monumandel3@gmail.com>
2025-06-04 21:24:26 +0530

    implement companionform and render it on companion new page

--- app/(root)/companions/new/page.tsx
+++ app/(root)/companions/new/page.tsx
@@ -1,7 +1,15 @@
+import Companionform from "@/components/Companionform";
 import React from "react";
 
-const page = () => {
-  return <div>page</div>;
+const Newcompanion = () => {
+  return (
+    <main className="min-lg:w-1/3 min-md:w-2/3 items-center justify-center ">
+      <article className="w-full gap-4 flex flex-col">
+        <h1>Companion Maker </h1>
+        <Companionform />
+      </article>
+    </main>
+  );
 };
 
-export default page;
+export default Newcompanion;
