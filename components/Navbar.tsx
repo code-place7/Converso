386227a30f911dff948347a24e78169718bed8e5
MONU MANDAL <monumandel3@gmail.com>
2025-06-05 20:31:03 +0530

    implement authentication using clerk and create a custom sign-in page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
-import Navitems from "./Navitems";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import Navitems from "@/components/Navitems";
 
 const Navbar = () => {
   return (
@@ -12,6 +13,14 @@ const Navbar = () => {
       </Link>
       <div className="flex items-center gap-8">
         <Navitems />
+        <SignedOut>
+          <SignInButton>
+            <button className="btn-signin">Sign In</button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </div>
     </nav>
   );
