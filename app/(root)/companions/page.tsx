316bd1d77d0463bdd9aa996ec416d351bb7f429f
MONU MANDAL <monumandel3@gmail.com>
2025-06-06 20:57:21 +0530

    implement getAllCompanions in companion.action and use it to show to users

--- app/(root)/companions/page.tsx
+++ app/(root)/companions/page.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+import { getAllCompanions } from "@/lib/actions/companion.action";
+import Companioncard from "@/components/Companioncard";
+import { getSubjectColor } from "@/lib/utils";
 
-const page = () => {
-  return <div>page</div>;
+const CompanionsLibrary = async ({ searchParams }: SearchParams) => {
+  const filters = await searchParams;
+  const subject = filters.subject ? filters.subject : "";
+  const topic = filters.topic ? filters.topic : "";
+
+  const companions = await getAllCompanions({ subject, topic });
+
+  return (
+    <main>
+      <section className="flex justify-between gap-4 max-sm:flex-col">
+        <h1>Companion Library</h1>
+      </section>
+      <section className="companions-grid">
+        {companions.map((companion) => (
+          <Companioncard
+            key={companion.id}
+            {...companion}
+            color={getSubjectColor(companion.subject)}
+          />
+        ))}
+      </section>
+    </main>
+  );
 };
 
-export default page;
+export default CompanionsLibrary;
